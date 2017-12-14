7d7e215c287598704713c5fc1338fc4eebe71a33
muktiwbowo <muktiwbowo@gmail.com>
2017-12-15 00:21:08 +0700

    post kodeobat dan kodepenyakit

--- app/src/main/java/com/muktiwbowo/skripsweet/konsul/KonsulActivity.java
+++ app/src/main/java/com/muktiwbowo/skripsweet/konsul/KonsulActivity.java
@@ -5,12 +5,8 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -18,11 +14,8 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.guna.libmultispinner.MultiSelectionSpinner;
 import com.muktiwbowo.skripsweet.R;
 import com.muktiwbowo.skripsweet.Url;
 
@@ -35,21 +28,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class KonsulActivity extends AppCompatActivity implements MultiSelectionSpinner.OnMultipleItemsSelectedListener {
+import io.apptik.widget.multiselectspinner.MultiSelectSpinner;
 
-    private static final String url = "https://dabudabu.000webhostapp.com/farnotifphp/ceksimilarity.php";
-    private static final String urlObat = "https://dabudabu.000webhostapp.com/farnotifphp/getobat.php";
-    private static final String urlGejala = "https://dabudabu.000webhostapp.com/farnotifphp/getgejala.php";
-    RequestQueue requestQueue, requestQueueObat, requestQueueGejala;
-    EditText edtPenyakit;
+public class KonsulActivity extends AppCompatActivity {
+
+    RequestQueue requestQueue, reqObat, reqGejala, reqPenyakit;
     Button btnCheck;
-    private TextView temp;
-    String tempgejala, tempidgejala;
-    MultiSelectionSpinner spinnerGejala, spinnerObat;
-    ArrayList<String> konsulObats, konsulGejala;
-    JSONArray resultObat, resultGejala;
-    List<KonsulGejala> namaNamaGejala;
-    List<String> idGejalaDipilih;
+    MultiSelectSpinner spinnerGejala, spinnerObat, spinnerPenyakit;
+    ArrayList<String> obat, gejala, penyakit;
+    JSONArray resultObat, resultGejala, resultPenyakit;
+    List<KonsulGejala> listGejala;
+    List<KonsulObat> listObat;
+    List<KonsulPenyakit> listPenyakit;
+    List<String> idGejalaDipilih, idObatDipilih, idPenyakitDipilih;
     private ProgressDialog progressDialog;
 
     @Override
@@ -59,72 +50,85 @@ public class KonsulActivity extends AppCompatActivity implements MultiSelectionS
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         requestQueue = Volley.newRequestQueue(this);
-        requestQueueObat = Volley.newRequestQueue(this);
-        requestQueueGejala = Volley.newRequestQueue(this);
+        reqObat = Volley.newRequestQueue(this);
+        reqGejala = Volley.newRequestQueue(this);
+        reqPenyakit = Volley.newRequestQueue(this);
+
         progressDialog = new ProgressDialog(this);
 
-        konsulObats = new ArrayList<String>();
-        konsulGejala = new ArrayList<String>();
+        obat = new ArrayList<String>();
+        gejala = new ArrayList<String>();
+        penyakit = new ArrayList<String>();
+        listGejala = new ArrayList<>();
+        listObat = new ArrayList<>();
+        listPenyakit = new ArrayList<>();
 
-        temp = (TextView) findViewById(R.id.temp);
-        spinnerGejala = (MultiSelectionSpinner) findViewById(R.id.spinerGejala);
-        spinnerObat = (MultiSelectionSpinner) findViewById(R.id.spinerObat);
-        edtPenyakit = (EditText) findViewById(R.id.edt_penyakit);
+        spinnerGejala = (MultiSelectSpinner) findViewById(R.id.spinerGejala);
+        spinnerPenyakit = (MultiSelectSpinner) findViewById(R.id.spinerPenyakit);
+        spinnerObat = (MultiSelectSpinner) findViewById(R.id.spinerObat);
         btnCheck = (Button) findViewById(R.id.btn_check);
-        getSpinnerObat();
+
         getSpinnerGejala();
+        getSpinnerPenyakit();
+        getSpinnerObat();
+
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showResult();
-                //showDialog();;
+                progressDialog.setMessage("Processing...");
+                progressDialog.show();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                        Url.urlRekomendasi, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        AlertDialog.Builder alertDialogBuilder =
+                                new AlertDialog.Builder(KonsulActivity.this);
+                        alertDialogBuilder.setTitle("Result");
+                        alertDialogBuilder.setMessage(response);
+                        alertDialogBuilder
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        AlertDialog alertDialog = alertDialogBuilder.create();
+                        alertDialog.show();
+                        //Toast.makeText(KonsulActivity.this, response, Toast.LENGTH_SHORT).show();
+                        //Log.d("Response", response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(KonsulActivity.this, error.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<String, String>();
+                        for (int i = 0; i < idGejalaDipilih.size(); i++) {
+                            params.put("id_gejala[" + i + "]", idGejalaDipilih.get(i));
+                        }
+                        for (int j = 0; j < idObatDipilih.size(); j++) {
+                            params.put("kode_obat[" + j + "]", idObatDipilih.get(j));
+                        }
+                        for (int k = 0; k < idPenyakitDipilih.size(); k++) {
+                            params.put("kode_penyakit[" + k + "]", idObatDipilih.get(k));
+                        }
+                        return params;
+                    }
+                };
+                requestQueue.add(stringRequest);
             }
         });
     }
 
-    public void showResult(){
-        progressDialog.setMessage("Processing...");
-        progressDialog.show();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, Url.urlRekomendasi, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                progressDialog.dismiss();
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(KonsulActivity.this);
-                alertDialogBuilder.setTitle("Result");
-                alertDialogBuilder.setMessage(response);
-                alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                AlertDialog alertDialog = alertDialogBuilder.create();
-                alertDialog.show();
-                //Toast.makeText(KonsulActivity.this, response, Toast.LENGTH_SHORT).show();
-                //Log.d("Response", response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                progressDialog.dismiss();
-                Toast.makeText(KonsulActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                for(int i = 0;i<idGejalaDipilih.size();i++){
-                    params.put("id_gejala["+i+"]",idGejalaDipilih.get(i));
-                }
-                return params;
-            }
-        };
-        requestQueue.add(stringRequest);
-    }
-
     private void getSpinnerGejala() {
-        StringRequest requestGejala = new StringRequest(urlGejala, new Response.Listener<String>() {
+        StringRequest requestGejala = new StringRequest(Url.urlGejala, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
@@ -138,33 +142,101 @@ public class KonsulActivity extends AppCompatActivity implements MultiSelectionS
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Toast.makeText(KonsulActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-        requestQueueGejala.add(requestGejala);
+        reqGejala.add(requestGejala);
     }
 
     private void getDataGejala(JSONArray resultGejala) {
-        namaNamaGejala = new ArrayList<>();
-        for (int i=0; i<resultGejala.length(); i++){
+        for (int i = 0; i < resultGejala.length(); i++) {
             try {
                 JSONObject obj = resultGejala.getJSONObject(i);
-                konsulGejala.add(obj.getString("nama_gejala"));
+                gejala.add(obj.getString("nama_gejala"));
                 KonsulGejala gejala = new KonsulGejala();
                 gejala.setIdGejala(obj.getString("id_gejala"));
                 gejala.setNamaGejala(obj.getString("nama_gejala"));
-                namaNamaGejala.add(gejala);
+                listGejala.add(gejala);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-        spinnerGejala.setItems(konsulGejala);
-        spinnerGejala.setSelection(new int[]{0});
-        spinnerGejala.setListener(this);
+        spinnerGejala.setItems(gejala).setListener(new MultiSelectSpinner.MultiSpinnerListener() {
+            @Override
+            public void onItemsSelected(boolean[] selected) {
+
+                idGejalaDipilih = new ArrayList<>();
+                //StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < selected.length; i++) {
+                    if (selected[i]) {
+                        //builder.append(gejala.get(i).getIdGejala()).append(" ");
+                        idGejalaDipilih.add(listGejala.get(i).getIdGejala());
+                    }
+                }
+            }
+        })
+                .setAllCheckedText("Semua Terpilih")
+                .setAllUncheckedText("Pilih Gejala")
+                .setSelectAll(false)
+                .setTitle("Pilih Gejala");
     }
 
-    public void getSpinnerObat(){
-        StringRequest requestObat = new StringRequest(urlObat, new Response.Listener<String>() {
+    private void getSpinnerPenyakit() {
+        StringRequest requestPenyakit = new StringRequest(Url.urlPenyakit, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject object = new JSONObject(response);
+                    resultPenyakit = object.getJSONArray("penyakit");
+                    getDataPenyakit(resultPenyakit);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(KonsulActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        reqPenyakit.add(requestPenyakit);
+    }
+
+    private void getDataPenyakit(JSONArray resultPenyakit) {
+        for (int i = 0; i < resultPenyakit.length(); i++) {
+            try {
+                JSONObject obj = resultPenyakit.getJSONObject(i);
+                penyakit.add(obj.getString("nama_penyakit"));
+                KonsulPenyakit p = new KonsulPenyakit();
+                p.setIdPenyakit(obj.getString("kode_penyakit"));
+                p.setNamaPenyakit(obj.getString("nama_penyakit"));
+                listPenyakit.add(p);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        spinnerPenyakit.setItems(penyakit).setListener(new MultiSelectSpinner.MultiSpinnerListener() {
+            @Override
+            public void onItemsSelected(boolean[] selected) {
+                idPenyakitDipilih = new ArrayList<>();
+                //StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < selected.length; i++) {
+                    if (selected[i]) {
+                        //builder.append(gejala.get(i).getIdGejala()).append(" ");
+                        idPenyakitDipilih.add(listPenyakit.get(i).getIdPenyakit());
+                    }
+                }
+            }
+        })
+                .setAllCheckedText("Semua Penyakit Terpilih")
+                .setAllUncheckedText("Pilih Penyakit")
+                .setSelectAll(false)
+                .setTitle("Pilih Penyakit");
+    }
+
+    public void getSpinnerObat() {
+        StringRequest requestObat = new StringRequest(Url.urlObat, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
@@ -181,50 +253,44 @@ public class KonsulActivity extends AppCompatActivity implements MultiSelectionS
 
             }
         });
-        requestQueueObat.add(requestObat);
+        reqObat.add(requestObat);
     }
 
     private void getDataObat(JSONArray resultObat) {
-        for(int i=0; i< resultObat.length(); i++){
+        for (int i = 0; i < resultObat.length(); i++) {
             try {
                 JSONObject obj = resultObat.getJSONObject(i);
-                konsulObats.add(obj.getString("nama_obat"));
+                obat.add(obj.getString("nama_obat"));
+                KonsulObat obat = new KonsulObat();
+                obat.setIdObat(obj.getString("kode_obat"));
+                obat.setNamaObat(obj.getString("nama_obat"));
+                listObat.add(obat);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
         }
-        spinnerObat.setItems(konsulObats);
-        spinnerObat.setSelection(new int[]{0});
-        spinnerObat.setListener(this);
+        spinnerObat.setItems(obat).setListener(new MultiSelectSpinner.MultiSpinnerListener() {
+            @Override
+            public void onItemsSelected(boolean[] selected) {
+                idObatDipilih = new ArrayList<>();
+                //StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < selected.length; i++) {
+                    if (selected[i]) {
+                        //builder.append(gejala.get(i).getIdGejala()).append(" ");
+                        idObatDipilih.add(listObat.get(i).getIdObat());
+                    }
+                }
+            }
+        })
+                .setAllCheckedText("Semua Terpilih")
+                .setAllUncheckedText("Pilih Obat")
+                .setSelectAll(false)
+                .setTitle("Pilih Obat");
     }
-
-
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return super.onSupportNavigateUp();
-    }
-
-    @Override
-    public void selectedIndices(List<Integer> indices) {
-    }
-
-    @Override
-    public void selectedStrings(List<String> strings) {
-        tempidgejala = strings.toString();
-        idGejalaDipilih = new ArrayList<>();
-        for(int i =0;i<namaNamaGejala.size();i++){
-            for (String s : strings){
-                if(s.equals(namaNamaGejala.get(i).getNamaGejala())){
-                    idGejalaDipilih.add(namaNamaGejala.get(i).getIdGejala());
-                    break;
-                }
-            }
-
-        }
-         //Toast.makeText(this, String.valueOf(strings.size()), Toast.LENGTH_SHORT).show();
-        //temp.setText(tempidgejala);
     }
 }
---
+++ app/src/main/java/com/muktiwbowo/skripsweet/konsul/KonsulPenyakit.java
@@ -0,0 +1,25 @@
+package com.muktiwbowo.skripsweet.konsul;
+
+/**
+ * Created by muktiwbowo on 14/12/17.
+ */
+
+public class KonsulPenyakit {
+    private String namaPenyakit, idPenyakit;
+
+    public String getNamaPenyakit() {
+        return namaPenyakit;
+    }
+
+    public void setNamaPenyakit(String namaPenyakit) {
+        this.namaPenyakit = namaPenyakit;
+    }
+
+    public String getIdPenyakit() {
+        return idPenyakit;
+    }
+
+    public void setIdPenyakit(String idPenyakit) {
+        this.idPenyakit = idPenyakit;
+    }
+}
